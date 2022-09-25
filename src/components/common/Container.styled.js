e6635e6fd063a3fde3f42a7b90ab6ff03d9b7a45
Ihor Kolontaievskiy <92229337+iSIayer@users.noreply.github.com>
2022-09-25 19:49:36 +0300

    delete bg img

--- src/components/common/Container.styled.js
+++ src/components/common/Container.styled.js
@@ -7,7 +7,4 @@ export const Container = styled.div`
   justify-content: center;
   align-items: center;
   padding: 30px;
-  background-image: url(https://thumbs.dreamstime.com/z/contact-icon-isolated-abstract-blue-background-illustration-design-contact-icon-abstract-blue-background-illustration-design-167324468.jpg);
-  background-repeat: no-repeat;
-  background-size: contain;
 `;
