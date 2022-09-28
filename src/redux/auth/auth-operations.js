5b4bbb29aff1d48697651b994003bc27e99c5370
Ihor Kolontaievskiy <92229337+iSIayer@users.noreply.github.com>
2022-09-28 22:29:23 +0300

    update auth-oretions

--- src/redux/auth/auth-operations.js
+++ src/redux/auth/auth-operations.js
@@ -1,121 +1,3 @@
-// import axios from 'axios';
-// import { createAsyncThunk } from '@reduxjs/toolkit';
-// import { toast } from 'react-toastify';
-
-// axios.defaults.baseURL = 'https://connections-api.herokuapp.com';
-
-// const token = {
-//   set(token) {
-//     axios.defaults.headers.common.Authorization = `Bearer ${token}`;
-//   },
-//   unset() {
-//     axios.defaults.headers.common.Authorization = '';
-//   },
-// };
-
-// // Регистрация пользователя
-
-// const register = createAsyncThunk('auth/register', async credentials => {
-//   try {
-//     const { data } = await axios.post('/user/signup', credentials);
-//     token.set(data.token);
-//   } catch (error) {
-//     const responseError = error.response.status;
-
-//     switch (responseError) {
-//       case responseError === 400:
-//         toast.error('You are already registered, please try to login', {
-//           position: toast.POSITION.TOP_CENTER,
-//           theme: 'colored',
-//         });
-//         break;
-//       case responseError === 500:
-//         toast.error('Oops... Server error! Please try later!"', {
-//           position: toast.POSITION.TOP_CENTER,
-//           theme: 'colored',
-//         });
-//         break;
-//       default:
-//         toast.error('Something went wrong!');
-//     }
-//   }
-// });
-
-// // Вход пользователя в приложение
-
-// const logIn = createAsyncThunk('auth/login', async credentials => {
-//   try {
-//     const { data } = await axios.post('/users/login', credentials);
-//     token.set(data.token);
-//     return data;
-//   } catch (error) {
-//     const responseError = error.response.status;
-
-//     switch (responseError) {
-//       case responseError === 400:
-//         toast.error('Invalid address and/or password specified.', {
-//           position: toast.POSITION.TOP_CENTER,
-//           theme: 'colored',
-//         });
-//         break;
-//       default:
-//         toast.error('Something went wrong!');
-//     }
-//   }
-// });
-
-// // Выход пользователя из приложения
-
-// const logOut = createAsyncThunk('auth/logout', async credentials => {
-//   try {
-//     await axios.post('/users/logout', credentials);
-//     token.unset();
-//   } catch (error) {
-//     const responseError = error.response.status;
-
-//     switch (responseError) {
-//       case responseError === 500:
-//         toast.error('Oops... something happened to the server', {
-//           position: toast.POSITION.TOP_RIGHT,
-//           theme: 'dark',
-//         });
-//         break;
-//       default:
-//         toast.error('Something went wrong!');
-//     }
-//   }
-// });
-
-// //Действие при повторном обновлении страницы
-
-// const fetchCurrentUser = createAsyncThunk(
-//   'auth/refresh',
-//   async (_, thunkAPI) => {
-//     const state = thunkAPI.getState();
-//     const persistedToken = state.auth.token;
-//     if (!persistedToken) {
-//       return thunkAPI.rejectWithValue();
-//     }
-
-//     token.set(persistedToken);
-//     try {
-//       const { data } = await axios.get('users/current');
-//       return data;
-//     } catch {
-//       token.unset();
-//       toast.warn('Authorization timed out! Please authenticate again!');
-//     }
-//   }
-// );
-
-// const operations = {
-//   register,
-//   logOut,
-//   logIn,
-//   fetchCurrentUser,
-// };
-// export default operations;
-
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { toast } from 'react-toastify';
@@ -131,7 +13,7 @@ const token = {
   },
 };
 
-///Register User
+// Регистрация пользователя
 
 const register = createAsyncThunk('auth/register', async credentials => {
   try {
@@ -156,7 +38,7 @@ const register = createAsyncThunk('auth/register', async credentials => {
   }
 });
 
-//LogIn User
+// Вход пользователя в приложение
 
 const logIn = createAsyncThunk('auth/login', async credentials => {
   try {
@@ -174,7 +56,7 @@ const logIn = createAsyncThunk('auth/login', async credentials => {
   }
 });
 
-//LogOut User
+// Выход пользователя из приложения
 
 const logOut = createAsyncThunk('auth/logout', async credentials => {
   try {
@@ -193,7 +75,7 @@ const logOut = createAsyncThunk('auth/logout', async credentials => {
   }
 });
 
-//After refresh page
+//Действие при повторном обновлении страницы
 
 const fetchCurrentUser = createAsyncThunk(
   'auth/refresh',
